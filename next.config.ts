d2dd45955821ef91d86415310a97a4413bc2f479
Abdullah-dev0 <abdulah14200@gmail.com>
2025-12-12 14:42:25 +0500

    fix: update PostHog API host in instrumentation and config files

--- next.config.ts
+++ next.config.ts
@@ -13,11 +13,11 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/ingest/static/:path*',
+        source: '/ph/static/:path*',
         destination: 'https://us-assets.i.posthog.com/static/:path*',
       },
       {
-        source: '/ingest/:path*',
+        source: '/ph/:path*',
         destination: 'https://us.i.posthog.com/:path*',
       },
     ];
